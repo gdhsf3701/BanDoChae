53eaf626ec25c411ccc4ba217cfea0cd948daa60
박준서 <pppg12jintpt@gmail.com>
2024-08-19 17:56:12 +0900

    커밋

---
+++ Assets/01.Scripts/JS_DragSlotScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JS_DragSlotScript : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public static GameObject beingDraggedIcon;
+
+
+    Vector3 startPosition;
+
+    [SerializeField] Transform onDragParent;
+
+    [HideInInspector]
+    public Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        beingDraggedIcon = gameObject;
+
+        startPosition = transform.position;
+        startParent = transform.parent;
+
+        GetComponent<CanvasGroup>().blocksRaycasts=false;
+
+        transform.SetParent(onDragParent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        beingDraggedIcon = null;
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        if(transform.parent == onDragParent)
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent);
+        }
+    }
+}
